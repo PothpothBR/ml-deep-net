f2071f70f77f324f28ece3c6baffce181cff0a21
Bruno Dalagnol <dalagnolbruno88@gmail.com>
2023-03-31 08:28:46 -0300

    mudando para mnist

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
 from net import Net
-from scallers import min_max
+from scallers import min_max, color
 
+import tensorflow_datasets as tfds
+import tensorflow as tf
 import pandas as pd
 
-""" Informações sobre atributos do conjunto de dados 
+batch_size = 128
 
-Gravidezes: Para expressar o número de gestações
-Glicose: Para expressar o nível de glicose no sangue
-BloodPressure: Para expressar a medição da pressão arterial
-SkinThickness: Para expressar a espessura da pele
-Insulina: Para expressar o nível de insulina no sangue
-IMC: Para expressar o índice de massa corporal
-DiabetesPedigreeFunction: Para expressar a porcentagem de Diabetes
-Idade: Para expressar a idade
-Resultado: Para expressar o resultado final 1 é Sim e 0 é Não
-"""
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
 
-data = pd.read_csv("./diabetes_dataset.csv")
+ds_train = ds_train.map(color, num_parallel_calls=tf.data.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
 
-results = data[["Outcome"]]
-data = data.drop(["No", "Outcome"], axis=1)
+ds_test = ds_test.map(color, num_parallel_calls=tf.data.AUTOTUNE)
+ds_test = ds_test.cache()
 
-print(data)
+df_train: pd.DataFrame = tfds.as_dataframe(ds_train, ds_info)
+df_test: pd.DataFrame = tfds.as_dataframe(ds_test, ds_info)
 
+print(df_train.shape)
+df_train_batchs = [df_train.iloc[i:i+batch_size] for i in range(0, df_train.shape[0]-batch_size, batch_size)]
+df_train_batchs.append(df_train.iloc[len(df_train_batchs)*batch_size:])
 
-min_max(data, [
-    "Glucose",
-    "BloodPressure",
-    "SkinThickness",
-    "Insulin",
-    "BMI",
-    "Age"
-])
+df_train.reset_index()
 
+for i in df_train.iterrows():
+    print(i[2])
+
+exit()
 n = Net([8, 6, 4, 1], 0.08)
-n.load("diabetes")
 
 for entry, expect in zip(data.values, results.values):
     n.fit(entry, expect)
--- scallers.py
+++ scallers.py
@@ -1,3 +1,8 @@
+import tensorflow as tf
 def min_max(data, column):
     data[column] = (data[column] - data[column].min()) / (data[column].max() - data[column].min())     
     return data
+
+def color(image, label):
+  """Normalizes images: `uint8` -> `float32`."""
+  return tf.cast(image, tf.float32) / 255., label
